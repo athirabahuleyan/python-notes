2a4d4250c17f348170f335e4f42d1d242840c823
athirabahuleyan <athirabahuleyan28@gmail.com>
2019-03-25 20:23:19 +1100

    the different types of data analyzed and type casting included the user input fuction added to basic subtraction

---
+++ datatypes.py
@@ -0,0 +1,7 @@
+
+a = '10'
+c = int(a)
+b = c + 5
+print(b)
+a = " athira"
+print(a)
---
+++ user_input_add.py
@@ -0,0 +1,15 @@
+def sub(a, b):
+    diff = a - b
+    return diff
+
+a = input("enter first number:")
+b = input("enter second number:")
+a = int(a)
+b = int(b)
+q = sub(a, b)
+print("The difference of two numbers is:")
+print(q)
+print()
+p = input("enter your name:")
+print("The name entered is:")
+print(p)
